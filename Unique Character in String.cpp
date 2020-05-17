84ebbd6f4afba2458cf8d393ba950d507a9285e8
Pankhuri Bhatnagar <46318882+pankhuri25@users.noreply.github.com>
2020-05-18 00:04:23 +0530

    DAY 5
    Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
    
    Examples:
    
    s = "leetcode"
    return 0.
    
    s = "loveleetcode",
    return 2.

---
+++ Unique Character in String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> mp;
+        for(char c : s)
+        {
+            mp[c]++;
+        }
+        for(int i=0; i<s.length(); i++)
+        {
+            if(mp[s[i]]==1)
+                return i;
+        }
+        return -1;
+    }
+};
